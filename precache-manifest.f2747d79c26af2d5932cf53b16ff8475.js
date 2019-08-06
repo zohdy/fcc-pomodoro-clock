3a8a3d969743cd827099a7aace8946750c6d3bc7
Patrick Peter Zohdy <zohdy@Patricks-MacBook-Pro.local>
2019-08-06 14:32:33 +0200

    Updates

---
+++ precache-manifest.f2747d79c26af2d5932cf53b16ff8475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674fe46ec3a8f8dbe349ce3f146fb50c",
+    "url": "/fcc-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "b69d0899aeb9286f5113",
+    "url": "/fcc-pomodoro-clock/static/css/main.e73bd66e.chunk.css"
+  },
+  {
+    "revision": "c192c95b86a60fa743d1",
+    "url": "/fcc-pomodoro-clock/static/js/2.0cbdec33.chunk.js"
+  },
+  {
+    "revision": "b69d0899aeb9286f5113",
+    "url": "/fcc-pomodoro-clock/static/js/main.22d80308.chunk.js"
+  },
+  {
+    "revision": "ec7b801c7c9eecf06fdf",
+    "url": "/fcc-pomodoro-clock/static/js/runtime~main.a052a505.js"
+  }
+]);
